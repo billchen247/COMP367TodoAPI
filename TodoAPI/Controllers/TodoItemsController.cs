498bd4950cbeba7ec600602e00161cddc36bf718
Alice Huynh <mhuynh27@my.centennialcollege.ca>
2026-02-05 08:30:39 -0700

    Add quiz#2 to TodoItems; closes #1

--- TodoAPI/Controllers/TodoItemsController.cs
+++ TodoAPI/Controllers/TodoItemsController.cs
@@ -20,6 +20,7 @@ namespace TodoAPI.Controllers
             _context = context;
             if (_context.TodoItems.Count() == 0)
             {
+                _context.TodoItems.Add(new TodoItem { Name = "quiz2", IsComplete = true });
                 _context.TodoItems.Add(new TodoItem { Name="Lab1", IsComplete=true});
                 _context.TodoItems.Add(new TodoItem { Name="Lab2",IsComplete=false});
                 _context.TodoItems.Add(new TodoItem { Name="Lab3",IsComplete=false});
